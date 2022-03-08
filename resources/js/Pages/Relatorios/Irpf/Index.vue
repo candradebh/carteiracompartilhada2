cd47d17440a5d593bf9252466c477616f236c771
Carlos Andrade <dev_carlos@outlook.com>
2022-03-08 06:59:21 -0300

    relatorio de irpf

--- resources/js/Pages/Relatorios/Irpf/Index.vue
+++ resources/js/Pages/Relatorios/Irpf/Index.vue
@@ -6,13 +6,8 @@
         </template>
         <!--Subheader-->
         <template #subHeader>
-            Relátorio utilizado para declaração do Imposto de renda
-        </template>
-
-        <!--Content-->
-        <template #default>
             <t-form-content :reset-button="false" :submit-button="true" @submitted="save">
-                  <t-form-section
+                <t-form-section
                     title="Escolha o ano de referência">
                     <t-input-group class="col-span-12 md:col-span-6" label="Ano" labelFor="ano">
                         <t-input-select
@@ -25,13 +20,21 @@
                     </t-input-group>
                 </t-form-section>
             </t-form-content>
+        </template>
 
+        <!--Content-->
+        <template #default>
+            
+            <!-- Ultima posição do ano -->    
             <t-table :content="posicaoAnual" :header="tableHeader" :features="tableFeatures">
             </t-table>
-
-            <t-table :content="resultadosAcoes" :header="tableHeaderAcoes" :features="tableFeatures">
+        
+            <!-- Resultados das açoes -->   
+            <t-table :content="resultadosAcoes" :header="tableHeaderAcoes" :features="tableFeaturesResultados">
             </t-table>
             
+            <t-table :content="resultadosFii" :header="tableHeaderFiis" :features="tableFeaturesResultados">
+            </t-table>
            
         </template>
     </app-layout>
@@ -72,12 +75,21 @@ export default {
       { id: "total", label: "ToTal", key: "total", align: "right", status: true, sortable: true }
     ]);
     const tableHeaderAcoes = reactive([
-      { id: "mes", label: "mes", key: "mes", align: "center",   status: true, sortable: true },
-      { id: "compras", label: "compras", key: "compras", align: "left", status: true, sortable: true },
-      { id: "vendas", label: "vendas", key: "vendas", align: "right",  status: true, sortable: true },
-      { id: "resultado", label: "resultado", key: "resultado", align: "center", status: true, sortable: true },
-      { id: "prejuizoacumulado", label: "prejuizoacumulado", key: "prejuizoacumulado", align: "right", status: true, sortable: true }
+      { id: "mes", label: "mes", key: "mes", align: "center",   status: true, sortable: false },
+      { id: "compras", label: "compras", key: "compras", align: "left", status: true, sortable: false },
+      { id: "vendas", label: "vendas", key: "vendas", align: "right",  status: true, sortable: false },
+      { id: "resultado", label: "resultado", key: "resultado", align: "center", status: true, sortable: false },
+      { id: "prejuizoacumulado", label: "prejuizoacumulado", key: "prejuizoacumulado", align: "right", status: true, sortable: false }
     ]);
+
+     const tableHeaderFiis = reactive([
+      { id: "mes", label: "mes", key: "mes", align: "center",   status: true, sortable: false },
+      { id: "compras", label: "compras", key: "compras", align: "left", status: true, sortable: false },
+      { id: "vendas", label: "vendas", key: "vendas", align: "right",  status: true, sortable: false },
+      { id: "resultado", label: "resultado", key: "resultado", align: "center", status: true, sortable: false },
+      { id: "prejuizoacumulado", label: "prejuizoacumulado", key: "prejuizoacumulado", align: "right", status: true, sortable: false }
+    ]);
+    
     const tableFeatures = reactive({
       table: {
         design: "elegant",
@@ -85,7 +97,7 @@ export default {
         rowBorder: true,
         zebraRow: true,
         radius: 3,
-        perPage: 5
+        perPage: 12
       },
       pagination: {
         status: true,
@@ -104,7 +116,24 @@ export default {
       }
     });
 
-    return { tableHeader,tableHeaderAcoes, tableFeatures };
+    const tableFeaturesResultados = reactive({
+      table: {
+        design: "elegant",
+        seperatedRow: true,
+        rowBorder: true,
+        zebraRow: true,
+        radius: 3,
+        perPage: 12
+      },
+      pagination: {
+        status: false,
+        radius: 3,
+        range: 5,
+        jump: true,
+      }
+    });
+
+    return { tableHeader,tableHeaderAcoes, tableHeaderFiis, tableFeatures, tableFeaturesResultados };
   },
   methods: {
         save() {
